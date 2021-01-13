a282ef27b88a4f38489a2178c645e52e5a59aedd
Taslima Akter Projucti <projucti2k9@gmail.com>
2021-01-13 20:28:05 +0100

    First commit

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react"
+import ReactDOM from "react-dom"
+
+class Footer extends Component{
+    constructor(){
+        super()
+
+    }
+render(){
+    return(
+        <div>
+            <footer>Hello from Footer</footer>   
+        </div>
+        
+        )
+    }
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react"
+import ReactDOM from "react-dom"
+
+class Header extends Component{
+    constructor(){
+        super()
+
+    }
+render(){
+    return(
+        <div>
+            <header>Hello from Header</header>   
+        </div>
+        
+        )
+    }
+}
+
+export default Header
